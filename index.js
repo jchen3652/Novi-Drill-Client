c658b046eed431a320092707a349aa6e8105d68c
James Chen <jchen3652@gmail.com>
2019-08-04 19:55:28 -0400

    Boonk ganged into HTML

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+var express = require('express');
+var app = express();
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+// set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var port = process.env.PORT || 8080;
+
+// make express look in the public directory for assets (css/js/img)
+app.use(express.static(__dirname + '/public'));
+
+
+app.listen(port, function() {
+  console.log('Our app is running on http://localhost:' + port);
+});
