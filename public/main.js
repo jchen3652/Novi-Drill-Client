73cfae6a55334b8a368e244d06ccf54872d726c0
James Chen <jchen3652@gmail.com>
2019-08-04 20:53:19 -0400

    Begin work on text file reading

--- public/main.js
+++ public/main.js
@@ -26,4 +26,9 @@ function fetchIssues() {
 document.getElementById("showlife").addEventListener('click', function() {
   document.getElementById("message").innerHTML = "Your performer number is: " + document.getElementById("performerNumber").value;
   console.log("button pressed");
+  jQuery.get('http://localhost/data.txt', function(data) {
+    console.log(data)
+  });
+
+
 });
