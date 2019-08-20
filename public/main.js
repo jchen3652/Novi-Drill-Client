2703d604053c4cb549ad3d48a2e28fa89510577f
James Chen <jchen3652@gmail.com>
2019-08-20 01:29:29 -0400

    Remove sign of life

--- public/main.js
+++ public/main.js
@@ -1,10 +1,10 @@
-document.getElementById("showlife").addEventListener('click', function() {
-  document.getElementById("message").innerHTML = "Your performer number is: " + document.getElementById("performerNumber").value;
-  console.log("button pressed");
-  jQuery.get('http://novi-drill-client.herokuapp.com/data.txt', function(data) {
-    console.log(data);
-  });
-});
+// document.getElementById("showlife").addEventListener('click', function() {
+//   document.getElementById("message").innerHTML = "Your performer number is: " + document.getElementById("performerNumber").value;
+//   console.log("button pressed");
+//   jQuery.get('http://novi-drill-client.herokuapp.com/data.txt', function(data) {
+//     console.log(data);
+//   });
+// });
 
 document.getElementById("submit").addEventListener('click', function() {
   var url = "http://novi-drill-client.herokuapp.com/";
